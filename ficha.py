f913ae0c5cac49eac193354d7d4363e247f4f1bb
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-04-07 19:12:46 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -24,4 +24,55 @@ def ficha():
     #   - Código secreto: nombre invertido en mayúsculas
     #   - Las 3 notas, su suma, promedio y promedio entero
     #   - Cierre decorativo usando repetición de string ("=" * 24)
-    pass
+    
+    # Inputs
+    nombre = input().strip().title()
+    email = input().strip().lower()
+    nota1 = int(input().strip())
+    nota2 = int(input().strip())
+    nota3 = int(input().strip())
+
+    # Procesamiento del nombre
+    pos_espacio = nombre.find(" ")
+
+    nombre_pila = nombre[:pos_espacio]
+    apellido = nombre[pos_espacio + 1:]
+
+    iniciales = nombre_pila[0] + apellido[0]
+    usuario = f"{apellido.lower()}.{nombre_pila.lower()}"
+
+    # Email
+    email_valido = "@" in email
+    pos_arroba = email.find("@")
+    dominio = email[pos_arroba + 1:] if pos_arroba != -1 else ""
+
+    # Otros datos de strings
+    nombre_archivo = nombre.replace(" ", "_")
+    cantidad_a = nombre.lower().count("a")
+    codigo_secreto = nombre[::-1].upper()
+
+    # Notas
+    suma = nota1 + nota2 + nota3
+    promedio = suma / 3
+    promedio_entero = suma // 3
+
+    print("========================")
+    print("    FICHA DEL ALUMNO")
+    print("========================")
+    print(f"Nombre: {nombre}")
+    print(f"Email: {email}")
+    print(f"Caracteres en nombre: {len(nombre)}")
+    print(f"Iniciales: {iniciales}")
+    print(f"Usuario: {usuario}")
+    print(f"Email valido: {email_valido}")
+    print(f"Dominio: {dominio}")
+    print(f"Nombre para archivo: {nombre_archivo}")
+    print(f"Cantidad de a: {cantidad_a}")
+    print(f"Codigo secreto: {codigo_secreto}")
+    print(f"Nota 1: {nota1}")
+    print(f"Nota 2: {nota2}")
+    print(f"Nota 3: {nota3}")
+    print(f"Suma: {suma}")
+    print(f"Promedio: {promedio}")
+    print(f"Promedio entero: {promedio_entero}")
+    print("=" * 24)
