75122b9c1c6006f33124f087f59af2edf97eb1f0
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-04-07 19:17:30 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,10 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+    nombre =input()
+    apellido =input()
+    nombre_completo = f"{nombre} {apellido}"
+    print(nombre_completo.lower())
+    print(nombre_completo.title())
+    print(nombre_completo.upper())
+    print(f"\t{nombre_completo.lower()}")
